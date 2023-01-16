9f3caf6624f20343e258b1771d9087fbf3952d6a
Divyam_Sharma <divyamsharma822@gmail.com>
2023-01-16 15:24:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Home from "./pages/Home/Home";
+import ProductInfo from "./pages/ProductInfo/ProductInfo";
+import Products from "./pages/Products/Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <React.Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {/* // Home Route */}
+                    <Route index element={<Home />} />
+                    <Route path='/' element={<Home />} />
+
+                    <Route path='/Contact' element={<ContactUs />} />
+                    <Route path='/Products' element={<Products />} />
+                    <Route path='/ProductInfo' element={<ProductInfo />} />
+
+                    {/* // 404 Route */}
+                    <Route
+                        path='*'
+                        element={
+                            <div
+                                style={{ height: "80vh",fontSize:"28px",fontWeight:"600" }}
+                                className='d-flex justify-content-center align-items-center'
+                            >
+                               Page Not Found
+                            </div>
+                        }
+                    />
+                </Routes>
+            </React.Suspense>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
